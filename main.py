17f9d59fcf6b6c0025e2efe9c0abcc97d0bf0847
SistemMark5 <alekhin.ivan.alex@gmail.com>
2025-05-04 21:30:30 +0700

    connected static files and includes routers

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 import uvicorn
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+
+from src.config import settings
 from src.views import router as api_router
 
 app = FastAPI()
+
+app.mount("/static", StaticFiles(directory=settings.templates.static_path), name="static")
 
 @app.get("/")
 async def index():
