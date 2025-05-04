20b112dfa147ba6c05cfc48fbe13eee97b3df479
SistemMark5 <alekhin.ivan.alex@gmail.com>
2025-05-04 21:30:53 +0700

    create dependency for upload files

--- src/models/dependency.py
+++ src/models/dependency.py
@@ -1,10 +1,15 @@
+import os.path
+from datetime import datetime
+
 from fastapi import Path, HTTPException, status
 from fastapi.params import Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.sql.annotation import Annotated
+from starlette.datastructures import UploadFile
 
+from src.config import settings
 from src.models.db_helper import db_helper
-from src.models.repository import TaskRepository
+from src.models.crud import TaskRepository
 
 
 async def get_by_title(title: Annotated[str, Path], session: AsyncSession = Depends(db_helper.session_dependency)):
@@ -15,3 +20,11 @@ async def get_by_title(title: Annotated[str, Path], session: AsyncSession = Depe
     raise HTTPException(
         status_code=status.HTTP_404_NOT_FOUND
     )
+
+
+async def upload_files(file: UploadFile,):
+    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+    new_filename = f"{timestamp}_{file.filename}"
+    file_path = os.path.join(settings.files.file_dir, new_filename)
+    async with open(file_path, "wb") as f:
+        f.write(await file.read())
