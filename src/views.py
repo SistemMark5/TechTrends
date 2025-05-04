85fcec457f01d8715ab58af3fa6441adf6bfefb5
SistemMark5 <alekhin.ivan.alex@gmail.com>
2025-05-04 21:29:28 +0700

    create templates routers

--- src/views.py
+++ src/views.py
@@ -1,14 +1,38 @@
 from fastapi import APIRouter, Request
+from fastapi.params import Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+from starlette.datastructures import UploadFile
 
-from utils.templates import templates
+from src.models.crud import TaskRepository
+from src.models.db_helper import db_helper
+from src.utils.templates import templates
 
 router = APIRouter(prefix="/news")
 
-@router.get("")
+@router.get("/")
 async def news_page(
-        request: Request
+        request: Request,
+        session: AsyncSession = Depends(db_helper.session_dependency)
 ):
+    posts = await TaskRepository.get_posts(session=session)
+
     return templates.TemplateResponse(
         request=request,
         name="news.html",
+        context={"posts": posts}
     )
+
+@router.get("/create-post")
+async def create_post_page(
+        request: Request,
+):
+    return templates.TemplateResponse(
+        request=request,
+        name="make-post.html"
+    )
+
+@router.post("/create-post")
+async def create_post(
+        request: Request,
+):
+    pass
